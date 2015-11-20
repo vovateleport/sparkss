1fb309f6ccbfbaab537a1701bccd66581fa966a1
vkorablev <vovateleport@gmail.com>
2015-11-20 15:12:52 +0300

    +

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,27 +1,59 @@
 import datr.{Query, MarketLog, NetLog}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+import SparkContext._
 import com.github.nscala_time.time.Imports._
 import com.github.nscala_time.time.OrderingImplicits.DateTimeOrdering
 
 object App {
+  val urlSpark = "spark://ip-172-31-22-13:7077"
+  val pathLog = "/home/ubuntu/data/"
+  val pathConverted = "/run/shm/"
+  val fileNet = "net_prices-flat"
+  val fileMarket = "market_prices-flat"
+
+
   def main(args: Array[String]) {
-    val suff = args match { case Array("100") => "_100.csv" case _=>".csv.gz" }
+    val suff = if (args contains "100") "_100.csv" else ".csv.gz"
 
-    val c = new SparkConf()
-      .set("spark.task.maxFailures","1")
-
-    val sc = new SparkContext("spark://ip-172-31-22-13:7077","spark1",c)
+    val c = new SparkConf().set("spark.task.maxFailures","1")
+    val sc = new SparkContext(urlSpark,"spark1",c)
 
     try {
-      val baseNet:RDD[String] = sc.textFile(s"/home/ubuntu/data/net_prices-flat$suff").repartition(sc.defaultParallelism*3)
-      //val baseMarket:RDD[String] = sc.textFile(s"/home/ubuntu/data/market_prices-flat$suff").repartition(sc.defaultParallelism*3)
+      val (nc, mc) = args.contains("convert") match {
+        case true =>
+          val baseNet:RDD[String] = sc.textFile(pathLog + fileNet + suff).repartition(sc.defaultParallelism*3)
+          val baseMarket:RDD[String] = sc.textFile(pathLog + fileMarket + suff).repartition(sc.defaultParallelism*3)
+          //println("rows count net,market", baseNet.count(), baseMarket.count())
+          val nc0 = baseNet.map(NetLog.parse).filter(_.nonEmpty).map(_.get)
+          val mc0 = baseMarket.map(MarketLog.parse).filter(_.nonEmpty).map(_.get)
+
+          nc0.saveAsObjectFile(pathConverted+fileNet)
+          mc0.saveAsObjectFile(pathConverted+fileMarket)
+          (nc0, mc0)
+        case false =>
+          val baseNet:RDD[String] = sc.textFile(pathLog + fileNet + suff).repartition(sc.defaultParallelism*3)
+          val baseMarket:RDD[String] = sc.textFile(pathLog + fileMarket + suff).repartition(sc.defaultParallelism*3)
+          //println("rows count net,market", baseNet.count(), baseMarket.count())
+          val nc0 = baseNet.map(NetLog.parse).filter(_.nonEmpty).map(_.get)
+          val mc0 = baseMarket.map(MarketLog.parse).filter(_.nonEmpty).map(_.get)
+
+          //nc0.saveAsSequenceFile(pathConverted+fileNet)
+          //mc0.saveAsSequenceFile(pathConverted+fileMarket)
+          (nc0, mc0)
+      }
+
+      //val baseNet:RDD[String] = sc.textFile(pathLog + fileNet + suff).repartition(sc.defaultParallelism*3)
+      //val baseMarket:RDD[String] = sc.textFile(pathLog + fileMarket + suff).repartition(sc.defaultParallelism*3)
+
       //println("rows count net,market", baseNet.count(), baseMarket.count())
 
-      val nc = baseNet.map(NetLog.parse).filter(_.nonEmpty).map(_.get)
-      println("parsed logs count net", nc.count())//, mc.count())
+      //val nc = baseNet.map(NetLog.parse).filter(_.nonEmpty).map(_.get)
+      //val mc = baseMarket.map(MarketLog.parse).filter(_.nonEmpty).map(_.get)
 
-      println("net gds", listGds(nc))
+      //println("parsed logs count net, market", nc.count(), mc.count())
+
+     println("counts net market", nc.count(),mc.count())
 
     }finally {
       sc.stop()
