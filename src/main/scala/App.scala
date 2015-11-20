1d757da8adda83ca4951f0763f14768b8be4df91
vkorablev <vovateleport@gmail.com>
2015-11-20 14:02:33 +0300

    +

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -21,7 +21,7 @@ object App {
       val nc = baseNet.map(NetLog.parse).filter(_.nonEmpty).map(_.get)
       println("parsed logs count net", nc.count())//, mc.count())
 
-      println("net gds", listGds(nc))
+      //println("net gds", listGds(nc))
 
     }finally {
       sc.stop()
